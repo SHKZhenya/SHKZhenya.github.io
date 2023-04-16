a6ec793318acdb51bd966b6fa6bca507baafa351
SHKZhenya <96337438+SHKZhenya@users.noreply.github.com>
2023-04-16 22:42:10 +1200

    Create script.js

---
+++ script.js
@@ -0,0 +1,162 @@
+var russian = ['Андорра', 'ОАЭ', 'Афганистан', 'Антигуа и Барбуда', 'Ангилья', 'Албания', 'Армения', 'Ангола', 'Антарктика', 'Аргентина', 'Американское Самоа', 'Австрия', 'Австралия', 'Аруба', 'Аландские острова', 'Азербайджан', 'Босния и Герцеговина', 'Барбадос', 'Бангладеш', 'Бельгия', 'Буркина-Фасо', 'Болгария', 'Бахрейн', 'Бурунди', 'Бенин', 'Бермуды', 'Бруней', 'Боливия', 'Бразилия', 'Багамские Острова', 'Бутан', 'Остров Буве', 'Ботсвана', 'Белоруссия', 'Белиз', 'Канада', 'Кокосовые острова', 'ДР Конго', 'ЦАР', 'Республика Конго', 'Швейцария', 'Кот-д’Ивуар', 'Острова Кука', 'Чили', 'Камерун', 'Китай', 'Колумбия', 'Коста-Рика', 'Куба', 'Кабо-Верде', 'Кюрасао', 'Кипр', 'Чехия', 'Германия', 'Джибути', 'Дания', 'Доминика', 'Доминиканская Республика', 'Алжир', 'Эквадор', 'Эстония', 'Египет', 'Эритрея', 'Испания', 'Эфиопия', 'Финляндия', 'Фиджи', 'Фолклендские острова', 'Микронезия', 'Фарерские острова', 'Франция', 'Габон', 'Великобритания', 'Гренада', 'Грузия', 'Гвиана', 'Гернси', 'Гана', 'Гибралтар', 'Гренландия', 'Гамбия', 'Гвинея', 'Гваделупа', 'Экваториальная Гвинея', 'Греция', 'Гватемала', 'Гуам', 'Гвинея-Бисау', 'Гайана', 'Гонконг', 'Гондурас', 'Хорватия', 'Гаити', 'Венгрия', 'Индонезия', 'Ирландия', 'Израиль', 'Индия', 'Ирак', 'Иран', 'Исландия', 'Италия', 'Джерси', 'Ямайка', 'Иордания', 'Япония', 'Кения', 'Киргизия', 'Камбоджа', 'Кирибати', 'Коморы', 'Сент-Китс и Невис', 'КНДР', 'Республика Корея', 'Кувейт', 'Острова Кайман', 'Казахстан', 'Лаос', 'Ливан', 'Сент-Люсия', 'Лихтенштейн', 'Шри-Ланка', 'Либерия', 'Лесото', 'Литва', 'Люксембург', 'Латвия', 'Ливия', 'Марокко', 'Монако', 'Молдавия', 'Черногория', 'Сен-Мартен', 'Мадагаскар', 'Маршалловы Острова', 'Северная Македония', 'Мали', 'Мьянма', 'Монголия', 'Макао', 'Мавритания', 'Мальта', 'Маврикий', 'Мальдивы', 'Малави', 'Мексика', 'Малайзия', 'Мозамбик', 'Намибия', 'Нигер', 'Нигерия', 'Никарагуа', 'Нидерланды', 'Норвегия', 'Науру', 'Ниуэ', 'Новая Зеландия', 'Оман', 'Панама', 'Перу', 'Французская Полинезия', 'Папуа — Новая Гвинея', 'Филиппины', 'Пакистан', 'Польша', 'Сен-Пьер и Микелон', 'Острова Питкэрн', 'Пуэрто-Рико', 'Палестина', 'Португалия', 'Палау', 'Парагвай', 'Катар', 'Реюньон', 'Румыния', 'Сербия', 'Россия', 'Руанда', 'Саудовская Аравия', 'Соломоновы Острова', 'Сейшельские Острова', 'Судан', 'Швеция', 'Сингапур', 'Словения', 'Словакия', 'Сьерра-Леоне', 'Сан-Марино', 'Сенегал', 'Сомали', 'Суринам', 'Южный Судан', 'Сан-Томе и Принсипи', 'Сальвадор', 'Синт-Мартен', 'Сирия', 'Эсватини', 'Теркс и Кайкос', 'Чад', 'Того', 'Таиланд', 'Таджикистан', 'Токелау', 'Восточный Тимор', 'Туркмения', 'Тунис', 'Тонга', 'Турция', 'Тринидад и Тобаго', 'Тувалу', 'Китайская Республика', 'Танзания', 'Украина', 'Уганда', 'США', 'Уругвай', 'Узбекистан', 'Ватикан', 'Сент-Винсент и Гренадины', 'Венесуэла', 'Виргинские Острова (Великобритания)', 'Виргинские Острова (США)', 'Вьетнам', 'Вануату', 'Уоллис и Футуна', 'Самоа', 'Йемен', 'Майотта', 'ЮАР', 'Замбия', 'Зимбабве'];
+var code = ['ad', 'ae', 'af', 'ag', 'ai', 'al', 'am', 'ao', 'aq', 'ar', 'as', 'at', 'au', 'aw', 'ax', 'az', 'ba', 'bb', 'bd', 'be', 'bf', 'bg', 'bh', 'bi', 'bj', 'bm', 'bn', 'bo', 'br', 'bs', 'bt', 'bv', 'bw', 'by', 'bz', 'ca', 'cc', 'cd', 'cf', 'cg', 'ch', 'ci', 'ck', 'cl', 'cm', 'cn', 'co', 'cr', 'cu', 'cv', 'cw', 'cy', 'cz', 'de', 'dj', 'dk', 'dm', 'do', 'dz', 'ec', 'ee', 'eg', 'er', 'es', 'et', 'fi', 'fj', 'fk', 'fm', 'fo', 'fr', 'ga', 'gb', 'gd', 'ge', 'gf', 'gg', 'gh', 'gi', 'gl', 'gm', 'gn', 'gp', 'gq', 'gr', 'gt', 'gu', 'gw', 'gy', 'hk', 'hn', 'hr', 'ht', 'hu', 'id', 'ie', 'il', 'in', 'iq', 'ir', 'is', 'it', 'je', 'jm', 'jo', 'jp', 'ke', 'kg', 'kh', 'ki', 'km', 'kn', 'kp', 'kr', 'kw', 'ky', 'kz', 'la', 'lb', 'lc', 'li', 'lk', 'lr', 'ls', 'lt', 'lu', 'lv', 'ly', 'ma', 'mc', 'md', 'me', 'mf', 'mg', 'mh', 'mk', 'ml', 'mm', 'mn', 'mo', 'mr', 'mt', 'mu', 'mv', 'mw', 'mx', 'my', 'mz', 'na', 'ne', 'ng', 'ni', 'nl', 'no', 'nr', 'nu', 'nz', 'om', 'pa', 'pe', 'pf', 'pg', 'ph', 'pk', 'pl', 'pm', 'pn', 'pr', 'ps', 'pt', 'pw', 'py', 'qa', 're', 'ro', 'rs', 'ru', 'rw', 'sa', 'sb', 'sc', 'sd', 'se', 'sg', 'si', 'sk', 'sl', 'sm', 'sn', 'so', 'sr', 'ss', 'st', 'sv', 'sx', 'sy', 'sz', 'tc', 'td', 'tg', 'th', 'tj', 'tk', 'tl', 'tm', 'tn', 'to', 'tr', 'tt', 'tv', 'tw', 'tz', 'ua', 'ug', 'us', 'uy', 'uz', 'va', 'vc', 've', 'vg', 'vi', 'vn', 'vu', 'wf', 'ws', 'ye', 'yt', 'za', 'zm', 'zw'];
+var english = ['Andorra', 'UAE', 'Afghanistan', 'Antigua and Barbuda', 'Anguilla', 'Albania', 'Armenia', 'Angola', 'Antarctica', 'Argentina', 'American Samoa', 'Austria', 'Australia', 'Aruba', 'Aland Islands', 'Azerbaijan', 'Bosnia and Herzegovina', 'Barbados', 'Bangladesh', 'Belgium', 'Burkina Faso', 'Bulgaria', 'Bahrain', 'Burundi', 'Benin', 'Bermuda', 'Brunei', 'Bolivia', 'Brazil', 'Bahamas', 'Bhutan', 'Bouvet Island', 'Botswana', 'Belarus', 'Belize', 'Canada', 'Cocos Islands', 'DR Congo', 'CAR', 'Republic of Congo', 'Switzerland', 'Cat-Ivory Coast', 'Cook Islands', 'Chile', 'Cameroon', 'China', 'Colombia', 'Costa Rica', 'Cuba', 'Cape Verde', 'Curacao', 'Cyprus', 'Czech Republic', 'Germany', 'Djibouti', 'Denmark', 'Dominica', 'Dominican Republic', 'Algeria', 'Ecuador', 'Estonia', 'Egypt', 'Eritrea', 'Spain', 'Ethiopia', 'Finland', 'Fiji', 'Falkland Islands', 'Micronesia', 'Faroe Islands', 'France', 'Gabon', 'Great Britain', 'Grenada', 'Georgia', 'Guiana', 'Guernsey', 'Ghana', 'Gibraltar', 'Greenland', 'Gambia', 'Guinea', 'Guadeloupe', 'Equatorial Guinea', 'Greece', 'Guatemala', 'Guam', 'Guinea-Bissau', 'Guyana', 'Hong Kong', 'Honduras', 'Croatia', 'Haiti', 'Hungary', 'Indonesia', 'Ireland', 'Israel', 'India', 'Iraq', 'Iran', 'Iceland', 'Italy', 'Jersey', 'Jamaica', 'Jordan', 'Japan', 'Kenya', 'Kyrgyzstan', 'Cambodia', 'Kiribati', 'Comoros', 'Saint Kitts and Nevis', 'DPRK', 'Republic of Korea', 'Kuwait', 'Cayman Islands', 'Kazakhstan', 'Laos', 'Lebanon', 'Saint Lucia', 'Liechtenstein', 'Sri LankaLanka', 'Liberia', 'Lesotho', 'Lithuania', 'Luxembourg', 'Latvia', 'Libya', 'Morocco', 'Monaco', 'Moldova', 'Montenegro', 'Saint Martin', 'Madagascar', 'Marshall Islands', 'North Macedonia', 'Mali', 'Myanmar', 'Mongolia', 'Macau', 'Mauritania', 'Malta', 'Mauritius', 'Maldives', 'Malawi', 'Mexico', 'Malaysia', 'Mozambique', 'Namibia', 'Niger', 'Nigeria', 'Nicaragua', 'Netherlands', 'Norway', 'Nauru', 'Niue', 'New Zealand', 'Oman', 'Panama', 'Peru', 'French Polynesia', 'Papua — New Guinea', 'Philippines', 'Pakistan', 'Poland', 'Saint Pierre and Miquelon', 'Pitcairn Islands', 'Puerto Rico', 'Palestine', 'Portugal', 'Palau', 'Paraguay', 'Qatar', 'Reunion', 'Romania', 'Serbia', 'Russia', 'Rwanda', 'Saudi Arabia', 'Solomon Islands', 'Seychelles', 'Sudan', 'Sweden', 'Singapore', 'Slovenia', 'Slovakia', 'Sierra Leone', 'San Marino', 'Senegal', 'Somalia', 'Suriname', 'South Sudan', 'Sao Tome and Principe', 'El Salvador', 'Sint Maarten', 'Syria', 'Eswatini', 'Turks and Caicos', 'Chad', 'Togo', 'Thailand', 'Tajikistan', 'Tokelau', 'East Timor', 'Turkmenistan', 'Tunisia', 'Tonga', 'Turkey', 'Trinidad and Tobago', 'Tuvalu', 'Republic of China', 'Tanzania', 'Ukraine', 'Uganda', 'USA', 'Uruguay', 'Uzbekistan', 'Vatican City', 'Saint Vincent and the Grenadines', 'Venezuela', 'Virgin Islands (UK)', 'Virgin Islands (USA)', 'Vietnam', 'Vanuatu', 'Wallis and Futuna', 'Samoa', 'Yemen', 'Mayotte', 'South Africa', 'Zambia', 'Zimbabwe'];
+
+var yes = 0, no = 15, ans="ABOBA", a;
+var language = 0; //0 - русский   1 - английский
+var x = [0, 0, 0, 0];
+var s = [0, 0, 0, 0];
+var c = [0, 0, 0, 0];
+var id = [0, 0, 0, 0];
+var f = [0, 0, 0, 0];
+var canvas;
+var aa, bb;
+
+if (sessionStorage.getItem('x') != null) {
+    x = sessionStorage.getItem('x').split(',');
+    c = sessionStorage.getItem('c').split(',');
+    s = sessionStorage.getItem('s').split(',');
+    f = sessionStorage.getItem('s').split(',');
+    yes = +sessionStorage.getItem('yes');
+    no = +sessionStorage.getItem('no');
+    ans = +sessionStorage.getItem('ans');
+    a = +sessionStorage.getItem('a');
+    language = +sessionStorage.getItem('language');
+    aa = +sessionStorage.getItem('aa');
+    bb = +sessionStorage.getItem('bb');
+
+    document.getElementById('true').innerHTML = "Верно: " + yes;
+    document.getElementById('false').innerHTML = "Неверно: " + no;
+
+    canvas = document.getElementById('picture');
+    console.log(c);
+    console.log(a);
+    console.log(c[a]);
+    console.log(document.getElementById('picture'));
+    canvas.src = "/flag-icon-css/flags/4x3/" + c[a] + ".svg";
+
+    id[0] = document.getElementById('b0');
+    id[1] = document.getElementById('b1');
+    id[2] = document.getElementById('b2');
+    id[3] = document.getElementById('b3');
+
+    id[0].innerHTML = s[0];
+    id[1].innerHTML = s[1];
+    id[2].innerHTML = s[2];
+    id[3].innerHTML = s[3];
+
+    id[0].onclick = function() { Checker(f[0]) };
+    id[1].onclick = function() { Checker(f[1]) };
+    id[2].onclick = function() { Checker(f[2]) };
+    id[3].onclick = function() { Checker(f[3]) };
+}
+
+function randint(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; // [min; max)
+}
+  
+function Checker(check) {
+    canvas = document.getElementById('picture');
+    if (check == ans) {
+        yes += 1;
+    } else {
+        no += 1;
+    }
+    if (yes + no < 15) {
+        document.getElementById('true').innerHTML = "Верно: " + yes;
+        document.getElementById('false').innerHTML = "Неверно: " + no;
+        x[0] = randint(0, code.length);
+        x[1] = randint(0, code.length);
+        x[2] = randint(0, code.length);
+        x[3] = randint(0, code.length);
+        while (x[0] == x[1] || x[0] == x[2] || x[0] == x[3] || x[1] == x[2] || x[1] == x[3] || x[2] == x[3]) {
+            x[0] = randint(0, code.length);
+            x[1] = randint(0, code.length);
+            x[2] = randint(0, code.length);
+            x[3] = randint(0, code.length);
+        }
+        if (language == 0) {
+            s[0] = russian[x[0]];
+            s[1] = russian[x[1]];
+            s[2] = russian[x[2]];
+            s[3] = russian[x[3]];
+        } else {
+            s[0] = english[x[0]];
+            s[1] = english[x[1]];
+            s[2] = english[x[2]];
+            s[3] = english[x[3]];
+        }
+        c[0] = code[x[0]];
+        c[1] = code[x[1]];
+        c[2] = code[x[2]];
+        c[3] = code[x[3]];
+        a = randint(0, 4);
+        canvas = document.getElementById('picture');
+        canvas.src = "/flag-icon-css/flags/4x3/" + c[a] + ".svg";
+        ans = s[a];
+
+        id[0] = document.getElementById('b0');
+        id[1] = document.getElementById('b1');
+        id[2] = document.getElementById('b2');
+        id[3] = document.getElementById('b3');
+
+        id[0].onclick = function() { Checker(s[0]) };
+        id[1].onclick = function() { Checker(s[1]) };
+        id[2].onclick = function() { Checker(s[2]) };
+        id[3].onclick = function() { Checker(s[3]) };
+
+        id[0].innerHTML = s[0];
+        id[1].innerHTML = s[1];
+        id[2].innerHTML = s[2];
+        id[3].innerHTML = s[3];
+
+        f[0] = s[0];
+        f[1] = s[1];
+        f[2] = s[2];
+        f[3] = s[3];
+    } else if (yes + no == 15) {
+        document.getElementById('true').innerHTML = "Верно: " + yes;
+        document.getElementById('false').innerHTML = "Неверно: " + no;
+        id[0] = document.getElementById('b0');
+        id[1] = document.getElementById('b1');
+        id[2] = document.getElementById('b2');
+        id[3] = document.getElementById('b3');
+
+        id[0].onclick = function() { Checker('start') };
+        id[1].onclick = function() { Checker('start') };
+        id[2].onclick = function() { Checker('start') };
+        id[3].onclick = function() { Checker('start') };
+
+        f[0] = 'start';
+        f[1] = 'start';
+        f[2] = 'start';
+        f[3] = 'start';
+
+        id[0].innerHTML = "Заново";
+        id[1].innerHTML = "Заново";
+        id[2].innerHTML = "Заново";
+        id[3].innerHTML = "Заново";
+
+        s[0] = "Заново";
+        s[1] = "Заново";
+        s[2] = "Заново";
+        s[3] = "Заново";
+    } else {
+        yes = 0; no = -1;
+        Checker("start");
+    }
+    sessionStorage.setItem('x', x);
+    sessionStorage.setItem('c', c);
+    sessionStorage.setItem('s', s);
+    sessionStorage.setItem('yes', yes);
+    sessionStorage.setItem('no', no);
+    sessionStorage.setItem('ans', ans);
+    sessionStorage.setItem('a', a);
+    sessionStorage.setItem('language', language);
+    sessionStorage.setItem('canvas', canvas);
+    sessionStorage.setItem('f', f);
+    sessionStorage.setItem('aa', aa);
+    sessionStorage.setItem('bb', bb);
+}
